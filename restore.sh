2c2aebd59c3cb6df50d96338666cd1fdc0f99a45
lp1 <pi@lp1.eu>
2016-11-26 14:52:16 +0000

    Added save and restore scripts, updated confs

---
+++ restore.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp local/rules.v4 /etc/iptables/rules.v4
+cp local/dnsmasq.conf /etc/dnsmasq.conf
+cp local/hostapd.conf /etc/hostapd/hostapd.conf
+cp local/hostapd /etc/default/hostapd
+cp local/sysctl.conf /etc/sysctl.conf
---
+++ save.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir local
+cp /etc/iptables/rules.v4 local
+cp /etc/dnsmasq.conf local
+cp /etc/hostapd/hostapd.conf local
+cp /etc/default/hostapd local
+cp /etc/sysctl.conf local
