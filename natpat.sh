00a1d4f8b064916410753a4a038f12db679681cb
lp1 <pi@lp1.eu>
2017-09-19 21:36:03 +0000

    added nat_pat rules

--- natpat.sh
+++ natpat.sh
@@ -8,5 +8,5 @@
 # Short-Description: Start daemon at boot time
 # Description:       Enable service provided by daemon.
 ### END INIT INFO
-
+arp -s 10.0.0.246 D4:BE:D9:8F:E2:76
 sh /home/pi/AlphaRouter/nat_pat.rules.v4
